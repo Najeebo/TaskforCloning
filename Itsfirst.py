22d17f8dc24b97993e0c31c9e431595be5ab2bc3
najeeb Tareen <najeebtareen00@gmail.com>
2023-11-06 22:18:11 +0500

    branch edited

--- Itsfirst.py
+++ Itsfirst.py
@@ -1,1 +1,7 @@
 print("this file is of first branch and i have made some changes to it")
+print("this file is of first branch and i have made some changes to it")
+print("this file is of first branch and i have made some changes to it")
+print("this file is of first branch and i have made some changes to it")
+print("this file is of first branch and i have made some changes to it")
+print("this file is of first branch and i have made some changes to it")
+print("this file is of first branch and i have made some changes to it")
