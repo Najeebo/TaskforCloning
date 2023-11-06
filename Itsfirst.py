f98f88f431f151f80b4e32d87ae0b2d37641a918
najeeb Tareen <najeebtareen00@gmail.com>
2023-11-06 22:30:31 +0500

    Second change

--- Itsfirst.py
+++ Itsfirst.py
@@ -1,7 +1,3 @@
 print("this file is of first branch and i have made some changes to it")
 print("this file is of first branch and i have made some changes to it")
 print("this file is of first branch and i have made some changes to it")
-print("this file is of first branch and i have made some changes to it")
-print("this file is of first branch and i have made some changes to it")
-print("this file is of first branch and i have made some changes to it")
-print("this file is of first branch and i have made some changes to it")
